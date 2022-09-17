5c4c886c9fa38b89988b699e9f3033bc2558e2c1
1nchy <2493242629@qq.com>
2022-09-18 01:04:20 +0800

    reserve map at back / front

--- deque.hpp
+++ deque.hpp
@@ -72,15 +72,35 @@ template <typename _Tp, typename _Alloc> class deque : public deque_base<_Tp, _A
 
 
     /// modifiers
-    void push_back(const value_type& _x) {}
-    void push_back(value_type&& _x) {}
-    void pop_back() {}
+    void push_back(const value_type& _x) {
+        if (this->_data._finish._cur != this->_data._finish._last - 1) {
+            this->_M_construct_node(this->_data._finish._cur, _x);
+            ++this->_data._finish;
+        }
+        else {
+            _M_alloc_push_back(_x);
+        }
+    }
+    void push_back(value_type&& _x) {
+        emplace_back(std::move(_x));
+    }
+    void pop_back() {
+        if (this->_data._finish._cur != this->_data._finish._first) {
+            --this->_data._finish._cur;
+            this->_M_destory_node(this->_data._finish_cur);
+        }
+        else {
+
+        }
+    }
     void push_front(const value_type& _x) {}
-    void push_front(value_type&& _x) {}
+    void push_front(value_type&& _x) {
+        emplace_front(std::move(_x));
+    }
     void pop_front() {}
     template <typename... _Args> iterator emplace(const_iterator _pos, _Args&&... _args) {}
     template <typename... _Args> void emplace_back(_Args&&... _args) {}
-    template <typename... _Args> void emplate_front(_Args&&... _args) {}
+    template <typename... _Args> void emplace_front(_Args&&... _args) {}
     iterator insert(const_iterator _pos, const value_type& _e) {}
     template <typename _InputIterator> iterator insert(const_iterator _pos, _InputIterator _first, _InputIterator _last) {}
     iterator erase(const_iterator _pos) {}
@@ -100,8 +120,41 @@ template <typename _Tp, typename _Alloc> class deque : public deque_base<_Tp, _A
         os << ']';
         return os;
     }
+
+
+protected:
+    // to make sure at least %_nodes_to_add nodes exist at back
+    void _M_reserve_map_at_back(size_type _nodes_to_add = 1) {
+        if (_nodes_to_add + 1 > size_type(this->_data._map_size - (this->_data._finish._node - this->_data._map))) {
+            _M_reallocate_map();
+        }
+    }
+    // to make sure at least %_nodes_to_add nodes exist at front
+    void _M_reserve_map_at_front(size_type _nodes_to_add = 1) {
+        if (_nodes_to_add > size_type(this->_data._start._node - this->_data._map)) {
+            _M_reallocate_map();
+        }
+    }
+    void _M_reallocate_map() {
+
+    }
+
+    template <typename... _Args> void _M_alloc_push_back(_Args&&... _args) {
+
+    }
+    
 };
 
 };
+
+/*
+map memory model:
+    [%_map] [%_start] ... [%_finish] [] (%_map + %_map_size)
+    each one is a node, which contains %data_type::_S_buffer_size() elements
+
+iterator & node memory model:
+    node => [%_first] [%_cur] ... [] (%_last)
+    %iterator::_node => node
+*/
 
 #endif
