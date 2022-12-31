82c359ac718a769e9ed4864996f0847ea0d6702e
1nchy <2493242629@qq.com>
2022-12-31 16:34:14 +0800

    feat(demo): demo for associative container.

--- adebug.hpp
+++ adebug.hpp
@@ -2,11 +2,15 @@
 #define _ASP_DEBUG_HPP_
 
 #include <ostream>
+#include <iostream>
+#include <sstream>
 #include <functional>
 #include <map>
 #include <unordered_map>
 
 #include "basic_param.hpp"
+#include "log_utils.hpp"
+#include "type_traits.hpp"
 
 /**
  * automatic test of container.
@@ -32,7 +36,7 @@ template <typename _Container> struct debug_base {
     typedef typename _Container::iterator iterator;
     typedef typename _Container::const_iterator const_iterator;
 
-    typedef decltype(std::function<bool(const _Constainer& _l) const>) check_fptr;
+    typedef bool (*check_fptr)(const _Container&);
     typedef void (_Container::*clear_fptr)();
 
     typedef size_type (_Container::*size_fptr)() const;
@@ -41,8 +45,16 @@ template <typename _Container> struct debug_base {
     clear_fptr _clear;
     size_fptr _size;
 
-    debug(check_fptr _f) : _check(_f) {}
-    virtual ~debug() = default;
+    container_type _container;
+
+    debug_base() = default;
+    debug_base(check_fptr _f) : _check(_f) {}
+    virtual ~debug_base() = default;
+
+    virtual void demo() = 0;
+
+    // std::string _M_string_from_iterator(iterator _i) const;
+    std::string _M_string_from_iterator(const_iterator _i) const;
 };
 
 template <typename _SeqContainer> struct debug_seq_container : public debug_base<_SeqContainer> {
@@ -59,8 +71,11 @@ template <typename _SeqContainer> struct debug_seq_container : public debug_base
     typedef typename base::size_fptr size_fptr;
 /// member
     std::unordered_map<int, void_modify_fptr> _void_modify_map;
-    std::unordered_map<int, insert_fptr> _insert_map;
-    std::unordered_map<int, earse_fptr> _erase_map;
+    insert_fptr _insert_map;
+    earse_fptr _erase_map;
+
+/// demo function
+    void demo() override;
 };
 
 template <typename _AssoContainer> struct debug_asso_container : public debug_base<_AssoContainer> {
@@ -70,22 +85,96 @@ template <typename _AssoContainer> struct debug_asso_container : public debug_ba
     typedef typename base::value_type value_type;
     typedef typename base::iterator iterator;
     typedef typename base::const_iterator const_iterator;
-    typedef typename container_type::key_value key_value;
+    typedef typename container_type::key_type key_type;
 /// container function typedef
-    typedef iterator (container_type::*insert_fptr)(const key_value&, const value_type&);
-    typedef iterator (container_type::*earse_fptr)(const key_value&);
-    typedef iterator (container_type::*find_fptr)(const key_value&);
-    typedef const_iterator (container_type::*cfind_fptr)(const key_value&) const;
-    typedef size_type (container_type::*count_fptr)(const key_value&) const;
+    typedef iterator (container_type::*insert_fptr)(const value_type&);
+    typedef iterator (container_type::*earse_fptr)(const key_type&);
+    typedef iterator (container_type::*find_fptr)(const key_type&);
+    typedef const_iterator (container_type::*cfind_fptr)(const key_type&) const;
+    typedef size_type (container_type::*count_fptr)(const key_type&) const;
     typedef typename base::size_fptr size_fptr;
 /// member
-    std::unordered_map<int, insert_fptr> _insert_map;
-    std::unordered_map<int, earse_fptr> _erase_map;
+    insert_fptr _insert;
+    earse_fptr _erase;
     find_fptr _find;
     cfind_fptr _cfind;
     count_fptr _count;
+
+/// constructor
+    debug_asso_container() = default;
+    virtual ~debug_asso_container() = default;
+
+/// demo function
+    void demo() override;
 };
 
+
+template <typename _SC> void debug_seq_container<_SC>::demo() {
+
+};
+template <typename _AC> void debug_asso_container<_AC>::demo() {
+    key_type _k;
+    value_type _v;
+    char _op;
+    std::cout << '[' << typeid(asp::decay_t<_AC>).name() << "]:" << std::endl;
+    while (!std::cin.eof()) {
+        std::cin >> _op;
+        switch (_op) {
+        case 'i': {
+            std::cin>> _v;
+            if (_insert != nullptr) {
+                auto _p = (this->_container.*_insert)(_v);
+                std::cout << "insert(" << _v << ") = " << this->_M_string_from_iterator(_p) << std::endl;
+            }
+            std::cout << this->_container << std::endl;
+        }; break;
+        case 'e': {
+            std::cin >> _k;
+            if (_erase != nullptr) {
+                auto _p = (this->_container.*_erase)(_k);
+                std::cout << "erase(" << _k << ") = " << this->_M_string_from_iterator(_p) << std::endl;
+            }
+            std::cout << this->_container << std::endl;
+        }; break;
+        case 'c': {
+            if (this->_clear != nullptr) {
+                (this->_container.*this->_clear)();
+            }
+            std::cout << this->_container << std::endl;
+        }; break;
+        
+        default:
+
+            break;
+        }
+
+    }
+};
+
+
+/// output
+// template <typename _C> auto debug_base<_C>::_M_string_from_iterator(iterator _i) const
+// -> std::string {
+//     std::stringstream _ss;
+//     if (_i != this->_container.end()) {
+//         _ss << *_i;
+//     }
+//     else {
+//         _ss << "null";
+//     }
+//     return _ss.str();
+// };
+template <typename _C> auto debug_base<_C>::_M_string_from_iterator(const_iterator _i) const
+-> std::string {
+    std::stringstream _ss;
+    if (_i != this->_container.cend()) {
+        _ss << *_i;
+    }
+    else {
+        _ss << "null";
+    }
+    return _ss.str();
+};
 };
 
 #endif
--- log_utils.hpp
+++ log_utils.hpp
@@ -4,6 +4,9 @@
 #include <cstdio>
 #include <cstring>
 
+#include <ostream>
+#include <vector>
+
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 // #define __FILENAME__ __FILE__
 
@@ -17,5 +20,18 @@
 #define ASP_ERR(format, ...)\
  fprintf(__STD_ERROR__, "[%s][%s][%d]err: " format "\n",\
  __FILENAME__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+
+
+template <typename _Tp, typename _Alloc> std::ostream& operator<<(std::ostream& _os, const std::vector<_Tp, _Alloc> _v) {
+    _os << '[';
+    for (auto _p = _v.cbegin(); _p != _v.cend(); ++_p) {
+        _os << *_p;
+        if (_p + 1 != _v.cend()) {
+            _os << ", ";
+        }
+    }
+    _os << ']';
+    return _os;
+}
 
 #endif // _LOG_UTILS_HPP_
