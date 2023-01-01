5044a54571c90b7ea8bb9a4b63e122ca8d5e67a1
1nchy <2493242629@qq.com>
2023-01-01 14:53:44 +0800

    feat(unordered_map): class inheritance.

--- unordered_map.hpp
+++ unordered_map.hpp
@@ -4,6 +4,7 @@
 #include <functional>
 
 #include "basic_param.hpp"
+#include "hash_table.hpp"
 
 namespace asp {
 
@@ -12,11 +13,10 @@ template <typename _Key, typename _Value,
  typename _Alloc = std::allocator<_Value>
 > class unordered_map;
 
-template <typename _Key, typename _Value,
- typename _Hash,
- typename _Alloc
-> class unordered_map {
+template <typename _Key, typename _Value, typename _Hash, typename _Alloc>
+class unordered_map : public hash_table<_Key, std::pair<_Key, _Value>, _select_0x, _Hash, _Alloc> {
 public:
+    
 };
 
 };
