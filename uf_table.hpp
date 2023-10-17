6222cea23ce8393f15ed14606d857d46cfa8639b
1nchy <2493242629@qq.com>
2023-10-17 16:58:15 +0800

    feat(uf_table): copy and assign constructor

--- uf_table.hpp
+++ uf_table.hpp
@@ -2,12 +2,14 @@
 #define _ASP_UNION_FIND_TABLE_HPP_
 
 #include <cassert>
+#include <unordered_map>
 
+#include "associative_container_aux.hpp"
 #include "hash_table.hpp"
 #include "short_tree.hpp"
 
 namespace asp {
-template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash = std::hash<_Key>, typename _Alloc = std::allocator<_Value>> class uf_table;
+template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash = std::hash<_Key>, typename _Alloc = std::allocator<_Value>> struct uf_table;
 template <typename _Value, typename _Alloc> struct uf_table_alloc;
 
 template <typename _Tp> using uf_node = short_tree_node<_Tp>;
@@ -82,7 +84,7 @@ template <typename _Value, typename _Alloc> struct uf_table_alloc : public _Allo
 */
 
 template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc>
- class uf_table : public uf_table_alloc<_Value, _Alloc> {
+struct uf_table : public uf_table_alloc<_Value, _Alloc> {
     typedef uf_node<_Value> node_t;
     typedef hash_table<_Key, uf_node<_Value>*, _select_ufnode_action<_ExtKey>, true, _select_ufnode_action<_ExtValue>, _Hash, _Alloc> table_t;
     typedef uf_table_alloc<_Value, _Alloc> base;
@@ -92,6 +94,8 @@ template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, 
     typedef typename table_t::hash_code hash_code;
     typedef typename table_t::key_type key_type;
     typedef _Value value_type;
+    typedef asso_container::type_traits<_Value, true> _ContainerTypeTraits;
+    typedef typename _ContainerTypeTraits::mapped_type mapped_type;
 
     typedef hash_table<hash_code, std::pair<hash_code, tree_t*>, _select_0x, true, _select_1x, _Hash, _Alloc> sttable_t;
 
@@ -103,7 +107,8 @@ public:
     typedef typename table_t::const_iterator const_iterator;
 
     uf_table() = default;
-    uf_table(const self& _rhs) : _h(_rhs._h), _sth(_rhs._sth) {}
+    uf_table(const self& _rhs);
+    self& operator=(const self& _rhs);
     virtual ~uf_table() {}
 
     bool existed(const key_type& _k) const { return _h.count(_k) != 0; }
@@ -112,23 +117,63 @@ public:
     std::pair<bool, hash_code> delegate_id(const key_type& _k) const;
     std::pair<bool, hash_code> delegate_id(node_t* const _n) const;
     std::pair<bool, hash_code> delegate_id(const tree_t& _t) const;
-    size_type count() const { return _sth.size(); }
+    size_type classification() const { return _sth.size(); }
+    size_type size() const { return _h.size(); }
     bool sibling(const key_type& _x, const key_type& _y);
-    void add(const value_type& _v);
+    bool add(const value_type& _v);
     bool add(const key_type& _k, const value_type& _v);
     bool del(const key_type& _k);
     bool merge(const key_type& _x, const key_type& _y);
     bool elect(const key_type& _k);
+    bool empty() const { return _h.empty(); }
+    void clear();
 
 // check function
     int check() const;
+    void demo(std::istream& _is = std::cin, std::ostream& _os = std::cout);
 
 protected:
+    template <typename _NodeGen> void _M_assign(const self& _rhs, const _NodeGen& _gen);
     /**
      * @brief update tree information in %_sth
      * @details it's recommended that storing the hash_code of a tree before its any operation.
     */
     void _M_update_tree_info(hash_code _oc, tree_t& _t);
+};
+
+template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc>
+uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::uf_table(const self& _rhs) {
+/**
+ * assign each short_tree in %_sth to this, and construct %_h
+*/
+    _M_assign(_rhs, [this](const tree_t* _t) {
+        tree_t* _nt = this->_M_allocate_tree();
+        _nt->_M_assign(*_t, [this, _nt](const typename tree_t::node_type* _n) {
+            typename tree_t::node_type* _p = _nt->_M_allocate_node(*_n);
+            this->_h.insert(_p);
+            return _p;
+        });
+        hash_code _c = delegate_id(*_nt).second;
+        this->_sth.insert(std::make_pair(_c, _nt));
+        return _nt;
+    });
+};
+template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc> auto
+uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::operator=(const self& _rhs) -> self& {
+    if (&_rhs == this) return *this;
+    clear();
+    _M_assign(_rhs, [this](const tree_t* _t) {
+        tree_t* _nt = this->_M_allocate_tree(*_t);
+        _nt->_M_assign(*_t, [this, _nt](const typename tree_t::node_type* _n) {
+            typename tree_t::node_type* _p = _nt->_M_allocate_node(*_n);
+            this->_h.insert(_p);
+            return _p;
+        });
+        hash_code _c = delegate_id(*_nt).second;
+        this->_sth.insert(std::make_pair(_c, _nt));
+        return _nt;
+    });
+    return *this;
 };
 
 template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc> auto
@@ -166,16 +211,18 @@ uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::sibling(const key_typ
     return delegate_id(_x).second == delegate_id(_y).second;
 };
 template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc> auto
-uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::add(const value_type& _v) -> void {
+uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::add(const value_type& _v) -> bool {
+    if (existed(_ExtKey()(_v))) return false;
     tree_t* const _st = this->_M_allocate_tree();
     node_t* _n = _st->add(_v);
     hash_code _c = delegate_id(*_st).second;
     _sth.insert(std::make_pair(_c, _st));
     _h.insert(_n);
+    return true;
 };
 template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc> auto
 uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::add(const key_type& _k, const value_type& _v) -> bool {
-    if (!existed(_k)) return false;
+    if (!existed(_k) || existed(_ExtKey()(_v))) return false;
     tree_t& _t = *delegate(_k);
     node_t* const _n = _t.add(_v);
     _h.insert(_n);
@@ -185,9 +232,11 @@ template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, 
 uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::del(const key_type& _k) -> bool {
     if (!existed(_k)) return false;
     node_t* _n = *(_h.find(_k));
+    _h.erase(_k);
     tree_t& _t = *delegate(_n);
     hash_code _oc = delegate_id(_t).second;
     _t.del(_n);
+    if (!_t.empty()) _h.insert(_t.root());
     _M_update_tree_info(_oc, _t);
     return true;
 };
@@ -209,12 +258,29 @@ uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::elect(const key_type&
     node_t* _n = *(_h.find(_k));
     tree_t& _t = *delegate(_n);
     hash_code _oc = delegate_id(_t).second;
+    _h.erase(_k); _h.erase(_ExtKey()(_t.root()->val()));
     _t.elect(_n);
+    _h.insert(_n); _h.insert(_t.root());
     _M_update_tree_info(_oc, _t);
     return true;
 };
+template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc> auto
+uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::clear() -> void {
+    _h.clear();
+    for (auto _i = _sth.begin(); _i != _sth.end(); ++_i) {
+        (_i->second)->clear();
+    }
+    _sth.clear();
+};
 
 /// protected implementation
+template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc>
+template <typename _NodeGen> auto uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>
+::_M_assign(const self& _rhs, const _NodeGen& _gen) -> void {
+    for (auto _i = _rhs._sth.cbegin(); _i != _rhs._sth.cend(); ++_i) {
+        _gen(_i->second);
+    }
+};
 template <typename _Key, typename _Value, typename _ExtKey, typename _ExtValue, typename _Hash, typename _Alloc> auto
 uf_table<_Key, _Value, _ExtKey, _ExtValue, _Hash, _Alloc>::_M_update_tree_info(hash_code _oc, tree_t& _t) -> void {
     if (_t.empty()) {
